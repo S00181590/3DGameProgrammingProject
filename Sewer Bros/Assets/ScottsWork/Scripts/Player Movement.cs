d0c1c7ef913353c72a493bd17fd6e1554769ad40
Scott Gallagher <35927142+s00178222@users.noreply.github.com>
2020-01-02 20:23:05 +0000

    setup
    added main menu settings pause menu player movemnt and player input also began to add 2player

---
+++ Sewer Bros/Assets/ScottsWork/Scripts/Player Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float horizontal, vertical, rotate, zoom;
+
+    public float movemntspeed;
+    public float rotationSpeed;
+
+    Vector3 nextTranslation;
+    Vector3 nextRotation;
+
+    public Camera playerCamera;
+
+    public string HorizontalInput = "Horizontal";
+    public string VerticalInput = "Vertical";
+    public string RotationInput = "Rotate";
+   
+    // Update is called once per frame
+    void Update()
+    {
+        nextRotation = Vector3.zero;
+        nextTranslation = Vector3.zero;
+
+        horizontal = Input.GetAxisRaw(HorizontalInput);
+        vertical = Input.GetAxisRaw(VerticalInput);
+        rotate = Input.GetAxisRaw(RotationInput);
+
+        nextRotation.y = rotate * rotationSpeed;
+
+        nextTranslation.x = horizontal;
+        nextTranslation.z = vertical;
+        nextTranslation *= movemntspeed;
+        playerCamera.transform.LookAt(transform);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(nextRotation * Time.deltaTime);
+        if (nextTranslation != Vector3.zero)
+            transform.Translate((transform.forward + nextTranslation) * Time.deltaTime);
+    }
+}
---
+++ Sewer Bros/Assets/ScottsWork/Scripts/Settings.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public GameOptions gameOptions;
+
+    public Toggle fullScreenToggle;
+    public Dropdown resoutiondropdownsetting;
+    public Dropdown TextureQualitySettings;
+    public Slider MusicVolumeSlider;
+
+    public Resolution[] resolutions; //stores all resoultions and returns
+
+
+    public Button buttonapply;
+    public AudioSource Musicsources;
+
+    void OnEnable()
+    {
+        gameOptions = new GameOptions();
+
+        fullScreenToggle.onValueChanged.AddListener(delegate { SetFullScreen(); });
+        resoutiondropdownsetting.onValueChanged.AddListener(delegate { setResoultion(); });
+        TextureQualitySettings.onValueChanged.AddListener(delegate { setTextureQuality(); });
+        MusicVolumeSlider.onValueChanged.AddListener(delegate { SettingtheVolume(); });
+        buttonapply.onClick.AddListener(delegate { OnButtonApplyClick(); });
+
+
+        resoutiondropdownsetting.ClearOptions();
+        List<string> resoultionoptions = new List<string>();
+
+
+        resolutions = Screen.resolutions;
+
+        foreach (Resolution resolution in resolutions)
+        {
+            resoutiondropdownsetting.options.Add(new Dropdown.OptionData(resolution.ToString()));
+        }
+
+        LoadSettings();
+    }
+
+    public void setResoultion()
+    {
+        Screen.SetResolution(resolutions[resoutiondropdownsetting.value].width, resolutions[resoutiondropdownsetting.value].height, Screen.fullScreen);
+        gameOptions.ResolutionIndex = resoutiondropdownsetting.value;
+    }
+
+    public void SettingtheVolume()
+    {
+        Musicsources.volume = gameOptions.MusicVolume = MusicVolumeSlider.value;
+    }
+
+    public void SetFullScreen()
+    {
+
+        gameOptions.FullScreen = Screen.fullScreen = fullScreenToggle.isOn;
+
+    }
+
+    public void setTextureQuality()
+    {
+        QualitySettings.masterTextureLimit = TextureQualitySettings.value;
+    }
+
+    public void SaveSettings()
+    {
+        string jsonData = JsonUtility.ToJson(gameOptions, true);
+        File.WriteAllText(Application.persistentDataPath + "/gamesettings.json", jsonData);
+    }
+    public void OnButtonApplyClick()
+    {
+        SaveSettings();
+    }
+    public void LoadSettings()
+    {
+        gameOptions = JsonUtility.FromJson<GameOptions>(File.ReadAllText(Application.persistentDataPath + "/gamesettings.json"));
+        MusicVolumeSlider.value = gameOptions.MusicVolume;
+        resoutiondropdownsetting.value = gameOptions.ResolutionIndex;
+        TextureQualitySettings.value = gameOptions.TextureQualtity;
+        fullScreenToggle.isOn = gameOptions.FullScreen;
+        Screen.fullScreen = gameOptions.FullScreen;
+        resoutiondropdownsetting.RefreshShownValue();
+    }
+
+}
