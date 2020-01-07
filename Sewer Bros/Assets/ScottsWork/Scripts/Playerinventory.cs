05c84a53347f154a9ef6bd73bac48f69209b2157
Scott Gallagher <35927142+s00178222@users.noreply.github.com>
2020-01-07 18:58:37 +0000

    1
    updateing incentory

--- Sewer Bros/Assets/ScottsWork/Scripts/Playerinventory.cs
+++ Sewer Bros/Assets/ScottsWork/Scripts/Playerinventory.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Playerinventory : MonoBehaviour
 {
     public SaveableInventory Inventory = new SaveableInventory();
-    
+
     // for testing 
     #region 
+    public NpcInventory TestInventoryToBuyOff;
     public InventoryPanel testDisplay;
     void Start()
     {
@@ -24,16 +25,24 @@ public class Playerinventory : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.H))
         {
-            //Inventory.AddItem(0);
-            Inventory.AddItem("Health Potion");
+            Inventory.AddItem(0);
+            //Inventory.AddItem("Health Potion");
         }
 
         if (Input.GetKeyDown(KeyCode.M))
         {
-            //Inventory.AddItem(1);
-            Inventory.AddItem("Magic Potion");
+            Inventory.AddItem(1);
+            //Inventory.AddItem("Magic Potion");
         }
 
+        //But a health potion from the NPC
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Item item = TestInventoryToBuyOff.BuyItem(0);
+
+            if (item != null)
+                Inventory.AddItem(item);
+        }
         //saveing the players inventory 
         if (Input.GetKeyDown(KeyCode.S))
         {
