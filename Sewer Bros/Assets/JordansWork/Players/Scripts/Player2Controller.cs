4ab487ff430f59dd3496926b6f8ae3a59017b06e
S00181590 <36006911+S00181590@users.noreply.github.com>
2020-01-18 14:25:07 +0000

    Added splitscreen and mulltiplayer movement

---
+++ Sewer Bros/Assets/JordansWork/Players/Scripts/Player2Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+    public float speed = 1.5f;
+
+    float horizontal, vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(new Vector3(horizontal, 0, vertical));
+    }
+}
