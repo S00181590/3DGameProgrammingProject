99b9b06ba3a76ecf28dde05329c81c12402e52a1
S00181590 <36006911+S00181590@users.noreply.github.com>
2020-01-20 12:24:26 +0000

    Added InGameChat API
    Added the api to the repo

---
+++ InGameChat/InGameChat/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace InGameChat.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ InGameChat/InGameChat/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace InGameChat.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
